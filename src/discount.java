383c048a6a4aba310baa6dc3d253b1b2696729e8
AnnaYedoshina <yedoshina.java@gmail.com>
2023-03-16 18:48:00 +0300

    logic

--- src/discount.java
+++ src/discount.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class discount {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
@@ -6,14 +7,11 @@ public class discount {
         double productB = 20;
         double price = productB + productA;
         double discount_percent = 0.1;
-        if (price == productB + productA)
-        { price = (price * 0.9);}
+
         double discount_value = price * discount_percent;
         double new_price = price - discount_value;
         System.out.println(discount_value);
         System.out.println(new_price);
-
-
 
 
     }
